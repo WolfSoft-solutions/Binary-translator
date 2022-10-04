a51a36a36d3ffaa94d77517ed651898db6972777
Commander R <wmxavier2004@gmail.com>
2022-10-04 12:32:11 +0000

    globally defined the config, added 2 buttons to the help command and got rid of the guildId in the deploy command because it wans't needed

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const discord = require('discord.js');
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const client = new Client({ intents: [ GatewayIntentBits.Guilds ] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const config = require('./config.json');
 const fs = require('fs');
 
 client.commands = new Collection();
 global.client = client;
 global.discord = discord;
+global.config = config;
 
 // Here the code makes a command of every file in each folder in the commands folder
 const commandFiles = fs.readdirSync(`./commands/`).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
 }
 
 
@@ -21,12 +22,12 @@ const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'
 
 // Here the code makes an event of every file in the events folder
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
 client.login(config.token);
--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,37 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("help")
-        .setDescription("A help command :)"),
-    async execute(interaction) {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("A help command :)"),
+  async execute(interaction) {
 
-        const embed = new EmbedBuilder()
-            .setTitle("Need help?")
-            .setDescription("This bot was purely made to convert binary code into text and the other way around.\n\nHere's the list of all the commands i can do:\n\n1001001 100000 1101000 1101111 1110000 1100101 100000 1111001 1101111 1110101 100000 1101100 1101001 1101011 1100101 100000 1110100 1101000 1100101 100000 1100010 1101111 1110100")
-            .addFields(
-                {name: 'Commands', value: '`/translate`\n`/help`\n`/stats`'},
-                { name: '\u200B', value: '\u200B' },
-                {name: 'Created by', value: '**Commander R#9371** (<@271285474516140033>) | **The Lost Pack Development**'},
-            )
-            .setColor("#FFCE30")
-            .setFooter({ text: `Requested by: ${interaction.user.tag}`, iconURL: `${interaction.user.avatarURL({dynamic: true})}` });
+    const embed = new EmbedBuilder()
+      .setTitle("Need help?")
+      .setDescription("This bot was purely made to convert binary code into text and the other way around.\n\nHere's the list of all the commands i can do:\n\n1001001 100000 1101000 1101111 1110000 1100101 100000 1111001 1101111 1110101 100000 1101100 1101001 1101011 1100101 100000 1110100 1101000 1100101 100000 1100010 1101111 1110100")
+      .addFields(
+        { name: 'Commands', value: '`/translate`\n`/help`\n`/stats`' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Created by', value: '**Commander R#9371** (<@271285474516140033>) | **The Lost Pack Development**' },
+      )
+      .setColor("#FFCE30")
+      .setFooter({ text: `Requested by: ${interaction.user.tag}`, iconURL: `${interaction.user.avatarURL({ dynamic: true })}` });
 
-        interaction.reply({embeds: [embed]});
-    },
+
+    const row = new discord.ActionRowBuilder()
+      .addComponents(
+        new discord.ButtonBuilder()
+          .setLabel('Support')
+          .setURL(config.support)
+          .setStyle(5),
+      )
+      .addComponents(
+        new discord.ButtonBuilder()
+          .setLabel('Invite me!')
+          .setURL(config.invite)
+          .setStyle(5),
+      )
+
+    interaction.reply({ embeds: [embed], components: [row] });
+  },
 };
